18f6761c7858f6748d2fe0d9598a7af926c1ccaa
Elisabeth Ngo <elisab.ngo@gmail.com>
2022-09-29 16:47:02 +0200

    three dependancies

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import * as THREE from 'three'
+import { Scene } from 'three'
 
 export default function Home() {
+  const scene = new THREE.Scene();
+
   return (
     <div className={styles.container}>
       <Head>
